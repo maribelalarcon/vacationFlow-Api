648fdfc7caf1409002b10075aa601953b58b7c68
Maribel <maribelsoledadalarcon@gmail.com>
2026-03-30 22:11:32 +0200

    inicializar proyecto back

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
